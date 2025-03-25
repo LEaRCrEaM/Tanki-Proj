84c06818e2cd50275566d6374429dfa2e016c252
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2025-03-25 14:16:56 -0400

    Update binder.js

--- game/binder.js
+++ game/binder.js
@@ -1,7 +1,9 @@
 /*import { functions, binderFuncs } from '../utils/helper.js';
 import { config } from '../utils/config.js'
 import { utils } from '../utils/utils.js'
+import { eventListeners, setEventListeners, removeEventListeners } from './eventListeners.js';
 
+addEventListeners();
 var animationFrameId;
 function animationFrameFunc() {
     animationFrameId = requestAnimationFrame(animationFrameFunc);
@@ -441,6 +443,38 @@ var utils = {
     }
 };
 
+var eventListeners = [
+    {
+        type: 'keydown',
+        handle: function(e) {
+            if (e.shiftKey && e.location == 2) {
+                if (!config.hacks.airBreak.tank.position.x || !config.hacks.airBreak.tank.position.y || !config.hacks.airBreak.tank.position.z) {
+                    var pos = utils.tankPosition;
+                    config.hacks.airBreak.tank.position = {
+                        x: pos.c18_1,
+                        y: pos.d18_1,
+                        z: pos.e18_1,
+                    };
+                };
+                config.hacks.airBreak.enabled = !config.hacks.airBreak.enabled;
+            };
+        }
+    }
+];
+
+function setEventListeners() {
+    eventListeners.forEach(listener => {
+        document.addEventListener(listener.type, listener.handle);
+    });
+};
+
+function removeEventListeners() {
+    eventListeners.forEach(listener => {
+        document.removeEventListener(listener.type, listener.handle);
+    });
+};
+
+addEventListeners();
 var animationFrameId;
 function animationFrameFunc() {
     animationFrameId = requestAnimationFrame(animationFrameFunc);
